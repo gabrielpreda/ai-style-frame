600cad5b821a5a6e74ed1cc56d7c08742d0e8330
Gabriel Preda <gabi.preda@gmail.com>
2025-06-28 15:15:27 +0300

    deploy setup

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 # Purpose: To deploy the App to Cloud Run.
 
-# Google Cloud Project
-PROJECT=gemini-first-439812
+# Google Cloud Project (replace with your actual project ID)
+PROJECT=<YOUR_PROJECT_ID>
 
-# Google Cloud Region
-LOCATION=us-central1
+# Google Cloud Region (replace with your actuall region: where the Gemini model is allowed)
+LOCATION=<your_region>
 
 # Depolying app from source code
 sudo ~/google-cloud-sdk/bin/gcloud run deploy simple-app --source . --region=$LOCATION --project=$PROJECT --allow-unauthenticated
